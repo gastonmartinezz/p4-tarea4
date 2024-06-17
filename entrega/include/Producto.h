d444494da149d9c4cc92e8011cd6f74995d5fffe
Manuel Paredes <luisparedescuenca@gmail.com>
2024-06-17 19:50:37 -0300

    Setters de producto

--- entrega/include/Producto.h
+++ entrega/include/Producto.h
@@ -18,15 +18,23 @@ private:
 public:
     enum class Categoria { Ropa, Electrodomesticos, Otros }; // Enum declarado dentro de la clase Producto
 
-    Producto();
     Producto(int id, int stock, float precio, string nombre, string descripcion, Categoria categoria);
+    ~Producto();
+     
     int getId();
     int getStock();
     float getPrecio();
     string getNombre();
     string getDescripcion();
     ~Producto();
-    Vendedor getVendedor(); //hay que implementar este metodo en el .cpp xq es nuevo
+    Vendedor getVendedor(); 
+
+    void setId(int id);
+    void setStock(int stock);
+    void setPrecio(float precio);
+    void setNombre(const string& nombre);
+    void setDescripcion(const string& descripcion);
+    void setVendedor(const Vendedor& vendedor);
 };
 
 set<Producto *> productosSistema; //Set donde guardamos todos los productos del sistema, ordenados por ID (Codigo identificador)
--- entrega/src/producto.cpp
+++ entrega/src/producto.cpp
@@ -2,8 +2,8 @@
 
 Producto::Producto() : id(0), stock(0), precio(0.0f), nombre(""), descripcion("") {}
 
-Producto::Producto(int id, int stock, float precio, std::string nombre, std::string descripcion, Categoria categoria)
-    : id(id), stock(stock), precio(precio), nombre(nombre), descripcion(descripcion) {
+Producto::Producto(int id, int stock, float precio, std::string nombre, std::string descripcion, Categoria categoria, const Vendedor& vendedor)
+    : id(id), stock(stock), precio(precio), nombre(nombre), descripcion(descripcion), vendedor(vendedor) {
     // Aquí deberías inicializar la categoría, pero como no la estás usando, la puedes eliminar
 }
 
@@ -16,5 +16,31 @@ float Producto::getPrecio() { return precio; }
 std::string Producto::getNombre() { return nombre; }
 
 std::string Producto::getDescripcion() { return descripcion; }
+
+Vendedor Producto::getVendedor() const { return vendedor; }
+
+void Producto::setId(int id) {
+    this->id = id;
+}
+
+void Producto::setStock(int stock) {
+    this->stock = stock;
+}
+
+void Producto::setPrecio(float precio) {
+    this->precio = precio;
+}
+
+void Producto::setNombre(const string& nombre) {
+    this->nombre = nombre;
+}
+
+void Producto::setDescripcion(const string& descripcion) {
+    this->descripcion = descripcion;
+}
+
+void Producto::setVendedor(const Vendedor& vendedor) {
+    this->vendedor = vendedor;
+}
 
 Producto::~Producto() {}
