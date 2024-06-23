542e0282480f20292c1f61fb75762b4415941f44
Gaston <ezegaston2011@gmail.com>
2024-06-23 13:07:14 -0300

    cambios

--- include/CasosDeUso.h
+++ include/CasosDeUso.h
@@ -4,8 +4,5 @@ void altaUsuario();
 void suscribirseANotificaciones();
 void listaUsuarios();
 void cargaDatosDePrueba();
-<<<<<<< HEAD
 void RealizarCompra();
-=======
 void crearPromocion();
->>>>>>> f3518a7 (crearPromocion funcionando a medias)
